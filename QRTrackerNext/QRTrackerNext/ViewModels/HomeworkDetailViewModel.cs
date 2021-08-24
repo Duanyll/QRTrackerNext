219d15273bce194600a26e1a7d2ffb261ace2436
duanyll <duanyll@outlook.com>
2021-08-24 14:35:41 +0800

    添加手动登记作业

--- QRTrackerNext/QRTrackerNext/ViewModels/HomeworkDetailViewModel.cs
+++ QRTrackerNext/QRTrackerNext/ViewModels/HomeworkDetailViewModel.cs
@@ -43,6 +43,7 @@ namespace QRTrackerNext.ViewModels
         public Command LoadStudentsCommand { get; }
 
         public Command GoScanCommand { get; }
+        public Command ManualSubmitCommand { get; }
 
         public HomeworkDetailViewModel(string homeworkId)
         {
@@ -109,6 +110,37 @@ namespace QRTrackerNext.ViewModels
                 };
 
                 Shell.Current.Navigation.PushAsync(csPage);
+            });
+
+            ManualSubmitCommand = new Command(async () =>
+            {
+                var res1 = await UserDialogs.Instance.PromptAsync("输入姓氏或全名搜索需要手动录入的学生", "手动录入学生");
+                if (res1.Ok && !string.IsNullOrWhiteSpace(res1.Text))
+                {
+                    var students = allStudents.Where(i => i.Name.StartsWith(res1.Text.Trim())).ToList();
+                    if (students.Count > 0)
+                    {
+                        var res2 = await UserDialogs.Instance.ActionSheetAsync("选择学生", "取消", null, null, students.Select(i => i.Name).ToArray());
+                        var student = students.Find(i => i.Name == res2);
+                        if (student != null) {
+                            if (studentsNotSubmitted.Contains(student))
+                            {
+                                realm.Write(() =>
+                                {
+                                    var scan = realm.Add(new ScanLog()
+                                    {
+                                        student = student
+                                    });
+                                    homework.Scans.Add(scan);
+                                });
+                            }
+                            await UserDialogs.Instance.AlertAsync($"已登记 {student.Name}", "登记成功");
+                        }
+                    } else
+                    {
+                        await UserDialogs.Instance.AlertAsync($"没有找到名称包含 {res1.Text.Trim()} 的学生!", "查找失败");
+                    }
+                }
             });
         }
 
