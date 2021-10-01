7552b0eca7c9fa92cd9e43a43d0dc439ef2cf419
duanyll <duanyll@outlook.com>
2021-10-01 19:05:06 +0800

    splash screen

--- QRTrackerNext/QRTrackerNext.Android/MainActivity.cs
+++ QRTrackerNext/QRTrackerNext.Android/MainActivity.cs
@@ -16,7 +16,7 @@ using Acr.UserDialogs;
 
 namespace QRTrackerNext.Droid
 {
-    [Activity(Label = "QRTracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "QRTracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
